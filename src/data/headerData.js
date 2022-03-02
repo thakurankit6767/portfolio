f6451c5a551c09249e49db575c9460486de9445b
thakurankit6767 <thakurankt6767@gmail.com>
2022-03-02 12:50:31 +0530

    svfv

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Ankit Thakur',
     title: "Web Developer",
     desciption:"A Full-Stack Web Developer, proficient in MERN stack. A team player who is passionate about coding and web development. Looking forward to an exciting opportunity to work as a dynamic web developer in an exciting organization.",
-    image: 'https://avatars.githubusercontent.com/u/91532920?v=4',
+    image: 'https://avatars.githubusercontent.com/u/95959359?s…00&u=e3b634b1ba1ebf902aa20a46842f53c496608ad7&v=4',
     resumePdf: resume
 }
